cfbd78fc6c4cfd748b5f4960ba4474acd16d4817
Deepak NB <deepak.75333@purc.tu.edu.np>
2023-07-24 23:00:00 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,6 +20,25 @@ DOWNLOADER_MIDDLEWARES = {
 SCRAPEOPS_PROXY_ENABLED = True
 # SCRAPEOPS_PROXY_SETTINGS = {'country': 'us'}
 
+SCRAPEOPS_API_KEY = '9aa063c2-ee98-4b8c-9b21-4e331ea5c303'
+
+# Add In The ScrapeOps Monitoring Extension
+EXTENSIONS = {
+'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
+
+
+DOWNLOADER_MIDDLEWARES = {
+
+    ## ScrapeOps Monitor
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    
+    ## Proxy Middleware
+    'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
+
+
 # Add In The ScrapeOps Monitoring Extension
 EXTENSIONS = {
 'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
